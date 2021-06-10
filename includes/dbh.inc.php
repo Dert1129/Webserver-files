f4ada4a02cfd96651ba8d0f408be75d99524be0c
Dert1129 <nathancreger1111@gmail.com>
2021-06-10 15:02:25 -0400

    Removed Clutter

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,15 +1,9 @@
 <?php
-$conn_array = array(
-    "UID" => '\TIRPSNathanC',
-    "PWD" => "",
-    "Database"=>"Test Database",
-);
-$conn = sqlsrv_connect('DESKTOP-88VUL4I', $conn_array);
-if($conn){
-    echo "Connection established. <br />";
-}else{
-    echo "Connection could not be established.<br />";
-    die(print_r(sql_errors(),true));
-}
 
+//connect to database
+$serverName ="DESKTOP-88VUL4I";
+$connectionInfo = array("Database"=>"Test Database");
+$conn = sqlsrv_connect($serverName,$connectionInfo);
+
+//display all jobs
 ?>
--- index.php
+++ index.php
@@ -1,110 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <style>
-    table{
-        border: 2px solid black;
-        table-layout: fixed;
-        width: 200px;
-        border-collapse: collapse;
-        id="Select-b";
-    }
-    th, td{
-        border: 2px solid black;
-        width: 250px;
-        overflow: hidden;
-        border-collapse: collapse;
-    } 
-    tr:hover{background-color: grey;}
-    tr:nth-child(all) {background-color: #f2f2f2;}
-
-    h1{
-        color: rgb(0,100,255);
-    }
-    p.num1{
-        padding: 35px 70px;
-    }
-    .ellipsis{
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-    }
-    
-    .appadd {
-        white-space: nowrap;
-        overflow: hidden;
-        width: 180px;
-        height: 30px;
-        text-overflow: ellipsis; 
-}
-    #mySearch {
-        width: 100%;
-        font-size: 18px;
-        padding: 11px;
-        border: 1px solid #ddd;
-    }
-
-/* Style the navigation menu */
-    #myMenu {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-        border: 1px solid black;
-    }
-
-/* Style the navigation links */
-    #myMenu li a {
-        padding: 12px;
-        text-decoration: none;
-        color: black;
-        display: block
-    }
-
-    #myMenu li a:hover {
-        background-color: #eee;
-    }
-    </style>
-    <h1>
-        <p class="num1"> All Job Information </p>
-</h1>
-
-</head>
-
-<!--Define navigation menu format-->
-<div class="row">
-    <div class="left" style="background-color:#bbb;">
-    <h2>Click to Sort</h2>
-    <ul id="myMenu">
-     <li><a href="#">Due Date</a></li>
-     <li><a href="#">Customer</a></li>
-     <li><a href="#">Insert a new entry..</a></li>
-    </ul>
-    </div>
-
-<body style="background-color: rgb(214,214,214);">
-    <p1>
-    <div class="right" style="background-color:#ddd;">
-        <h2>Job Table</h2>
-    <table>
-        <th>Technician</th>
-        <th>Job_number</th>
-        <th>Due_Date</th>
-        <th>Customer</th>
-        <th>Part_number</th>
-        <th>Part_Description</th>
-        <th>Qty</th>
-</tr>
 <?php
+include_once('./includes/dbh.inc.php');
+include_once('./Style and cleanup/Table.html');
+include_once('./Style and cleanup/Navigation.html');
+include_once('./Style and cleanup/Pictures.html');
 
-//connect to database
-$serverName ="DESKTOP-88VUL4I";
-$connectionInfo = array("Database"=>"Test Database");
-$conn = sqlsrv_connect($serverName,$connectionInfo);
 
-//display all jobs
-?>
-<img src="../../Customer/2021/John Deere/Quotes/29227/Thumbnail/DZ121668.png" alt="test_thumbnail">
-<?php
 if(($result = sqlsrv_query($conn, "SELECT * FROM Job_Schedule_details;"))!==false){
     while ($row = sqlsrv_fetch_array($result)){
         echo "<tr>";
