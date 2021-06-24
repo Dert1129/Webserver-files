a9979d018d4fb2e2c072dac799e6bf010f0268d1
Dert1129 <nathancreger1111@gmail.com>
2021-06-24 15:06:47 -0400

    Added new columns and changed Due_Date type

--- Schemas/Job_Schedule_Details.sql
+++ Schemas/Job_Schedule_Details.sql
@@ -1,15 +1,17 @@
 DROP TABLE IF EXISTS Job_Schedule
 CREATE TABLE Job_Schedule(
-	Technician text,
+	Technician varchar(50),
 	Job_number varchar(50),
-	Due_Date varchar(50),
+	Due_Date DateTime(50),
 	Customer varchar(50),
 	Part_Number varchar(50),
 	Part_Description varchar(MAX),
 	Customer_PO varchar(50),
 	Qty text,
-	[$] money,
-	Value money,
+	[$] varchar(50),
+	Value varchar(50),
 	Product_Code varchar(50),
-	JSDID INT IDENTITY(1,1) PRIMARY KEY
+	JSDID INT IDENTITY(1,1) PRIMARY KEY,
+	Thumbnail varbinary(MAX),
+	Year varchar(50)
 );
