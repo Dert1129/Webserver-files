13b793f6fe0b8e0c0d4897e850d4091df81e42f7
Dert1129 <nathancreger1111@gmail.com>
2021-06-24 15:04:31 -0400

    Removed Home()

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ include_once('./includes/dbh.inc.php');
 //include_once('./Style and cleanup/Navigation.html');
 //include_once('./Style and cleanup/Pictures.php');
 include_once('./routes/home.php');
-Home();
+
 
 ?>
