d9683a032604e639aae08c6d4366a931c7977a24
Dert1129 <nathancreger1111@gmail.com>
2021-07-03 11:28:02 -0400

    Installed nodejs

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-include_once('./includes/dbh.inc.php');
+//include_once('./includes/dbh.inc.php');
 //include_once('./Style and cleanup/Navigation.html');
 include_once('./Style and cleanup/Pictures.php');
 include_once('./routes/home.php');
 //Update_Images();
+
 ?>
