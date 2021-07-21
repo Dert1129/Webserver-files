754ce1cac5d682dd31a69204b8324dbb9a26f679
Dert1129 <nathancreger1111@gmail.com>
2021-07-21 12:27:47 -0400

    Changed filepath

--- execute_sql.php
+++ execute_sql.php
@@ -7,7 +7,7 @@
     $mysql_password = "";
 
     $db = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
-    $sql = file_get_contents("//tiws07/dwg/Mfg Mtg/.csv/Do Everything Script.sql");
+    $sql = file_get_contents("//tiws07/dwg/Mfg Mtg/Nathan/Webserver Files/Schemas/Do everything Script.sql");
 
     $stmt = $db->prepare($sql);
 
