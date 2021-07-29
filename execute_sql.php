071962a1d74384e37b3d35aec9d0c15034843159
Dert1129 <nathancreger1111@gmail.com>
2021-07-29 15:18:46 -0400

    Remove whitespace

--- execute_sql.php
+++ execute_sql.php
@@ -5,12 +5,9 @@
     $mysql_database = "test";
     $mysql_user = "root";
     $mysql_password = "";
-
     $db = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
     $sql = file_get_contents("//tiws07/dwg/Mfg Mtg/Nathan/Webserver Files/Schemas/Do everything Script.sql");
-
     $stmt = $db->prepare($sql);
-
     if($stmt->execute()){
         echo "Success";
     }else{
