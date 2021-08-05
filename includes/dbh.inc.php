970d20e3ef90c7d804006d86fe91261b58c9310d
Dert1129 <nathancreger1111@gmail.com>
2021-08-05 09:49:50 -0400

    Changed dbname

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
     $servername = "127.0.0.1";
     $username = "root";
     $password = "";
-    $dbname = "test";
+    $dbname = "webserver";
     $conn = new mysqli($servername, $username, $password, $dbname);
     if(!$conn){
         die("Connection failed: ". mysqli_connect_error());
