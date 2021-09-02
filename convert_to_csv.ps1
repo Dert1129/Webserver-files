940d65f1e033d767982f2a42fd901656295466fa
Dert1129 <nathancreger1111@gmail.com>
2021-09-02 19:50:56 -0400

    Added remove-item & move-item

--- convert_to_csv.ps1
+++ convert_to_csv.ps1
@@ -1,14 +1,17 @@
 Function ExcelToCsv($File){
-    $myDir = "\\tiws07\dwg\Mfg Mtg\"
+    $myDir = "\\tiws07\dwg\Mfg Mtg\Customer Schedule\"
     $excelFile = "$myDir\" + $File + ".xlsx"
     $Excel = New-Object -ComObject Excel.Application
-    $wb = $Excel.wORKBOOKS.oPEN($ExcelFile)
+    $wb = $Excel.WORKBOOKS.oPEN($ExcelFile)
 
     foreach ($ws in $wb.Worksheets){
         $Excel.DisplayAlerts = $false;
+        $ws.Columns("C").NumberFormat = "yyyy-mm-dd"
         $ws.SaveAs("$myDir\" + $File + ".csv", 6)
     }
     $Excel.Quit()
 }
-$FileName = "Job Schedule"
+$FileName = "Job Schedule Details"
 ExcelToCsv -File $FileName
+Remove-Item -Path "C:\xampp\mysql\data\webserver\Job Schedule Details.csv" -Force
+Move-Item -Path "\\tiws07\dwg\Mfg Mtg\Customer Schedule\Job Schedule Details.csv" -Destination "C:\xampp\mysql\data\webserver\" -Force
