18af40e033f1beb9df445735447b43ec3438409e
Dert1129 <nathancreger1111@gmail.com>
2021-09-17 11:15:21 -0400

    Added support for Brake & Baltec.sql

--- execute_sql.php
+++ execute_sql.php
@@ -1,21 +1,25 @@
 <?php
+function Customer_Schedule($Filename){
     $mysql_host = "127.0.0.1";
     $mysql_database = "webserver";
     $mysql_user = "root";
     $mysql_password = "";
     $db = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
-    $sql = file_get_contents("//tiws07/dwg/Mfg Mtg/Nathan/Webserver Files/Schemas/Do everything Script.sql");
-    $stmt = $db->prepare($sql);
-    if($stmt->execute()){
-        echo "Success\n";
-    }else{
-        echo "Failed\n";
+        $sql = file_get_contents("//tiws07/dwg/Mfg Mtg/Nathan/Webserver Files/Schemas/".$Filename);
+        $stmt = $db->prepare($sql);
+        if($stmt->execute()){
+            echo "Success\n";
+        }else{
+            echo "Failed\n";
+        }
+        $dir = '//tiws07/dwg/Mfg Mtg/Nathan/Webserver files/';
+        if ( !file_exists($dir) ) {
+            mkdir ($dir, 0744);
+        }
     }
-    $dir = '//tiws07/dwg/Mfg Mtg/Nathan/Webserver files/';
-    if ( !file_exists($dir) ) {
-        mkdir ($dir, 0744);
-    }
+    Customer_Schedule("Do everything Script.sql");
+    Customer_Schedule("Brake & Baltec.sql");
     date_default_timezone_set("America/Detroit");
-    file_put_contents ($dir.'update.html', '<link href="./Style and cleanup/txtstyle.css" rel="stylesheet" type="text/css" />'."\n"."Last Updated: ".date("Y-m-d")." at ".date("g:iA"));
+    file_put_contents ('//tiws07/dwg/Mfg Mtg/Nathan/Webserver Files/update.html', '<link href="./Style and cleanup/txtstyle.css" rel="stylesheet" type="text/css" />'."\n"."Last Updated: ".date("Y-m-d")." at ".date("g:iA"));
     echo "Updated Timestamp";
 ?>
