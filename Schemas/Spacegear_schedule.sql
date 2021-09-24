7e305306ac0c85624caefd5bd4842997c7a21ba6
Dert1129 <nathancreger1111@gmail.com>
2021-09-24 09:52:36 -0400

    Added spacegear site

---
+++ Schemas/Spacegear_schedule.sql
@@ -0,0 +1,241 @@
+DROP TABLE IF EXISTS Spacegear_Schedule;
+CREATE TABLE Spacegear_Schedule(
+    Customer varchar(50),
+    Job_Number varchar(50),
+    Due_Date date,
+    Part_Number varchar(50),
+    Status varchar(50),
+    Revision varchar(50),
+    QtyLeft text
+);
+
+LOAD DATA INFILE "Job Schedule Detail-Spacegear.csv"
+INTO TABLE Spacegear_Schedule
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY "\r\n"
+IGNORE 1 ROWS;
+
+UPDATE Spacegear_Schedule
+SET Customer = "Ada Metals"
+WHERE Customer = "ADA Metal Products Inc";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Aeroparts'
+WHERE Customer = 'AeroParts Manufacturing & Repair';
+
+UPDATE Spacegear_Schedule
+SET Customer = "Alabama Laser"
+WHERE Customer = "Alabama Laser Systems";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Arvin Sango, Inc'
+WHERE Customer = 'ARVIN SANGO, INC';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'California Hydroforming'
+WHERE Customer = 'CALIFORNIA HYDROFORMING CO INC';
+
+UPDATE Spacegear_Schedule
+SET Customer = "Cardington Yutaka"
+WHERE Customer = "Cardington Yutaka Technologies";
+
+UPDATE Spacegear_Schedule
+SET Customer = "Carnegie Robotics"
+WHERE Customer = "Carnegie Robotics, LLC";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Case NH'
+WHERE Customer = 'CASE NEW HOLLAND';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Caterpillar'
+WHERE Customer = 'CATERPILLAR INC.';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Chip Ganassi Racing'
+WHERE Customer = 'CHIP GANASSI RACING';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Chrysler'
+WHERE Customer = 'CHRYSLER COMPANY LLC';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Cobra Patterns & Models'
+WHERE Customer = 'COBRA PATTERNS & MODELS INC';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'ContiTech'
+WHERE Customer = 'CONTITECH';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Cummins'
+WHERE Customer = 'CUMMINS EMISSION SOLUTIONS';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Depatie'
+WHERE Customer = 'DEPATIE FLUID POWER COMPANY';
+
+UPDATE Spacegear_Schedule
+SET Customer = "Eaton"
+WHERE Customer = "Eaton Corporation";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Eberspaecher'
+WHERE Customer = 'EBERSPAECHER N.A.';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Electrolux'
+WHERE Customer = 'Electro Lux';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Exhaust Systems'
+WHERE Customer = 'EXHAUST SYSTEMS';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Faurecia'
+WHERE Customer = 'FAURECIA EMISSIONS CONTROL';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Ford'
+WHERE Customer = 'FORD MOTOR COMPANY';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'GMP Metal Products'
+WHERE Customer = 'GMP METAL PRODUCTS';
+
+UPDATE Spacegear_Schedule 
+SET Customer = 'Hendrick Motorsports'
+WHERE Customer = "HENDRICK MOTORSPORTS";
+
+UPDATE Spacegear_Schedule 
+SET Customer = "Hendrickson Int'l"
+WHERE Customer = "Hendrickson International";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Honda'
+WHERE Customer = 'HONDA R&D AMERICAS';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Humanetics'
+WHERE Customer = 'HUMANETICS INNOVATIVE SOLUTION';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Isuzu'
+WHERE Customer = 'ISUZU MANUFACTURING SERVICES';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'JAC Products'
+WHERE Customer = 'JAC PRODUCTS';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Jedco'
+WHERE Customer = 'JEDCO, INC';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'John Deere'
+WHERE Customer = 'JOHN DEERE HARVESTER WORKS';
+
+UPDATE Spacegear_Schedule
+SET Customer = "Kellog"
+WHERE Customer = "Kellogg's";
+
+UPDATE Spacegear_Schedule
+SET Customer = "Mack Trucks"
+WHERE Customer = "Mack Trucks, Inc";
+
+UPDATE Spacegear_Schedule
+SET Customer = "Magneti"
+WHERE Customer = "Magneti Marelli";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Matcor'
+WHERE Customer = 'MATCOR MATSU';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'MSI Defense'
+WHERE Customer = 'MSI DEFENSE SOLUTIONS';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Newman Tech'
+WHERE Customer = 'NEWMAN TECHNOLOGY, INC';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Nikola'
+WHERE Customer = 'NIKOLA CORPORATION';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Penske'
+WHERE Customer = 'PENSKE RACING SOUTH INC';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Pit Products'
+WHERE Customer = 'PIT PRODUCTS LLC.';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Pridgeon & Clay'
+WHERE Customer = 'PRIDGEON & CLAY';
+
+UPDATE Spacegear_Schedule
+SET Customer = "ProFab"
+WHERE Customer = "ProFab Production Fabricators, Inc.";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Richard Childress Racing'
+WHERE Customer = 'RCR RACE OPERATIONS, LLC';
+
+UPDATE Spacegear_Schedule
+SET Customer = "Rivian"
+WHERE Customer = "RIVIAN";
+
+UPDATE Spacegear_Schedule
+SET Customer = "Senior Flexonics"
+WHERE Customer = "Senior Flexonics Inc";
+
+UPDATE Spacegear_Schedule
+SET Customer = "Senneker"
+WHERE Customer = "Senneker Performance";
+
+UPDATE Spacegear_Schedule
+SET Customer = "Stryker"
+WHERE Customer = "Stryker Corporation";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Technique Chassis'
+WHERE Customer = 'TECHNIQUE CHASSIS';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Tenneco'
+WHERE Customer = 'TENNECO GRASS LAKE';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Tenneco Inc'
+WHERE Customer = 'TENNECO, INC';
+
+UPDATE Spacegear_Schedule
+SET Customer = "Thor Sport Racing"
+WHERE Customer = "ThorSport Racing";
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Thyssenkrupp'
+WHERE Customer = 'THYSSENKRUPP NA';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Tramec Sloan'
+WHERE Customer = 'TRAMEC SLOAN(CAT)';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Volvo'
+WHERE Customer = 'VOLVO TRUCKS N. AMERICA';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'World Class Industries'
+WHERE Customer = 'WORLD CLASS INDUSTRIES';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'Yamaha'
+WHERE Customer = 'YAMAHA MOTOR MFG. CORP';
+
+UPDATE Spacegear_Schedule
+SET Customer = 'ZF NA'
+WHERE Customer = 'ZF NORTH AMERICA';
---
+++ routes/Spacegear_Schedule.php
@@ -0,0 +1,42 @@
+<?php 
+function spacegear(){
+    require('./includes/dbh.inc.php');
+    $stmt = $conn->prepare("SELECT * FROM Spacegear_Schedule;");
+    $stmt->execute();
+    $result = $stmt->get_result(); 
+    if ($result !==false){
+        while ($row = $result->fetch_assoc()){
+            $date = date_format(new DateTime($row["Due_Date"]), "Y-m-d");
+            $current_date = date("Y-m-d");
+            $year = date("Y");
+            $pastYear = $year - 1;
+            $path = '//tiws07/dwg/Customer/'.$year . '/' . $row['Customer'].'/Jobs/'.$row['Job_Number'];
+            $altPath = '//tiws07/dwg/Customer/'.$pastYear . '/' . $row['Customer'].'/Jobs/'.$row['Job_Number'];
+            if(is_dir($path)){
+                $directory = "file://///tiws07/dwg/Customer/".$year."/".$row['Customer']. "/Jobs/". $row['Job_Number'];
+                $job = "<a href=\"$directory"."\"> " . $row['Job_Number'] . " </a> </td>";
+            }elseif(is_dir($altPath)){
+                $directory = "file://///tiws07/dwg/Customer/".$pastYear."/".$row['Customer']. "/Jobs/". $row['Job_Number'];
+                $job = "<a href=\"$directory"."\"> " . $row['Job_Number'] . " </a> </td>";
+            }else{
+                $job = $row['Job_Number'];
+            }
+            echo "<tr>";
+            if($date < $current_date){
+                $text = "text-danger";
+            }else{
+                $text = "";
+            }
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Customer']. "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$job."</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$date. "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Part_Number']."</td>";  
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Status']."</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Revision']."</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['QtyLeft']. "</td>";
+        }
+    }else{
+        die("Connection failed: ". mysqli_connect_error());
+    }
+}
+?>
