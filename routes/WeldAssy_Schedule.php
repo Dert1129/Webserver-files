66f469b652562cbc1523f8f4fb0e4a32ab0930df
Dert1129 <nathancreger1111@gmail.com>
2021-09-24 10:49:59 -0400

    Implemented WeldAssy schedule

---
+++ routes/WeldAssy_Schedule.php
@@ -0,0 +1,43 @@
+<?php 
+function weld(){
+    require('./includes/dbh.inc.php');
+    $stmt = $conn->prepare("SELECT * FROM WeldAssy_Schedule;");
+    $stmt->execute();
+    $result = $stmt->get_result(); 
+    if ($result !==false){
+        while ($row = $result->fetch_assoc()){
+            $date = date_format(new DateTime($row["Due_Date"]), "Y-m-d");
+            $current_date = date("Y-m-d");
+            $year = date("Y");
+            $pastYear = $year - 1;
+            $path = '//tiws07/dwg/Customer/'.$year . '/' . $row['Customer'].'/Jobs/'.$row['Job_Number'];
+            $altPath = '//tiws07/dwg/Customer/'.$pastYear . '/' . $row['Customer'].'/Jobs/'.$row['Job_Number'];
+            if(is_dir($path)){
+                $directory = "file://///tiws07/dwg/Customer/".$year."/".$row['Customer']. "/Jobs/". $row['Job_Number'];
+                $job = "<a href=\"$directory"."\"> " . $row['Job_Number'] . " </a> </td>";
+            }elseif(is_dir($altPath)){
+                $directory = "file://///tiws07/dwg/Customer/".$pastYear."/".$row['Customer']. "/Jobs/". $row['Job_Number'];
+                $job = "<a href=\"$directory"."\"> " . $row['Job_Number'] . " </a> </td>";
+            }else{
+                $job = $row['Job_Number'];
+            }
+            echo "<tr>";
+            if($date < $current_date){
+                $text = "text-danger";
+            }else{
+                $text = "";
+            }
+            
+            echo "<td class='col-1 $text' style='height:8rem'>".$job."</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$date. "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Customer']. "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Part_Number']."</td>";  
+            echo "<td class='col-1 $text' style='height:8rem'>". mb_strimwidth($row['Part_Description'],0,15,'...'). "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['QtyLeft']. "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Technician']."</td>";
+        }
+    }else{
+        die("Connection failed: ". mysqli_connect_error());
+    }
+}
+?>
