8b843ad6ba141962c0bba9f18f713b03269950c8
Dert1129 <nathancreger1111@gmail.com>
2021-09-27 16:15:25 -0400

    Removd master Job Number pathing

--- routes/Tube_Bender.php
+++ routes/Tube_Bender.php
@@ -12,25 +12,13 @@ function Bender(){
             $pastYear = $year - 1;
             $path = '//tiws07/dwg/Customer/'.$year . '/' . $row['Customer'].'/Jobs/'.$row['Job_number'];
             $altPath = '//tiws07/dwg/Customer/'.$pastYear . '/' . $row['Customer'].'/Jobs/'.$row['Job_number'];
-            $MasterPath = '//tiws07/dwg/Customer/'.$year . '/' . $row['Customer'].'/Jobs/'.$row['Master_Job_Number'];
-            $altMasterPath = '//tiws07/dwg/Customer/'.$pastYear . '/' . $row['Customer'].'/Jobs/'.$row['Master_Job_Number'];
             if(is_dir($path)){
                 $directory = "file://///tiws07/dwg/Customer/".$year."/".$row['Customer']. "/Jobs/". $row['Job_number'];
                 $job = "<a href=\"$directory"."\"> " . $row['Job_number'] . " </a> </td>";
             }elseif(is_dir($altPath)){
                 $directory = "file://///tiws07/dwg/Customer/".$pastYear."/".$row['Customer']. "/Jobs/". $row['Job_number'];
                 $job = "<a href=\"$directory"."\"> " . $row['Job_number'] . " </a> </td>";
-            }elseif(is_dir($MasterPath)){
-                $MasterDirectory = "file://///tiws07/dwg/Customer/".$year."/".$row['Customer']. "/Jobs/". $row['Master_Job_Number'];
-                $MasterJob = "<a href=\"$MasterDirectory"."\"> " . $row['Master_Job_Number'] . " </a> </td>";
-            }elseif(is_dir($altMasterPath)){
-                $MasterDirectory = "file://///tiws07/dwg/Customer/".$year."/".$row['Customer']. "/Jobs/". $row['Master_Job_Number'];
-                $MasterJob = "<a href=\"$MasterDirectory"."\"> " . $row['Master_Job_Number'] . " </a> </td>";
-            }else{
-                $job = "Directory Not Yet Available <br> <br>".$row['Job_number'];
-                $MasterJob = $row['Master_Job_Number'];
-            }
-            echo "<tr>";
+            }echo "<tr>";
             if($date < $current_date){
                 $text = "text-danger";
             }else{
@@ -42,7 +30,7 @@ function Bender(){
             echo "<td class='col-1 $text' style='height:8rem'>". $row['Customer']. "</td>";
             echo "<td class='col-1 $text' style='height:8rem'>".$row['Part_Number']."</td>";   
             echo "<td class='col-1 $text' style='height:8rem'>".$row['Status']."</td>"; 
-            echo "<td class='col-1 $text' style='height:8rem'>".$MasterJob."</td>";            
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Master_Job_Number']."</td>";            
             echo "<td class='col-1 $text' style='height:8rem'>". $row['Qty']. "</td>";
         }
     }else{
