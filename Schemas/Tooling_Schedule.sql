1274a71b0470d0ca9a15439d3aab19c62b7ba9db
Dert1129 <nathancreger1111@gmail.com>
2021-09-28 17:29:02 -0400

    Implemented toolting schedule schema

---
+++ Schemas/Tooling_Schedule.sql
@@ -0,0 +1,249 @@
+DROP TABLE IF EXISTS Tooling_Schedule;
+CREATE TABLE Tooling_Schedule(
+    Customer varchar(50),
+	Job_number varchar(50),
+	Due_Date date,
+	Part_Number varchar(50),
+	Qty_To_Stock text,
+	Design_Complete varchar(50),
+	Work_Center varchar(50)
+);
+
+LOAD DATA INFILE "Job Schedule Detail-Tooling.csv"
+INTO TABLE Tooling_Schedule
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n"
+IGNORE 1 ROWS;
+
+UPDATE Tooling_Schedule
+SET Part_Number = replace(Part_Number,"/","");
+
+UPDATE Tooling_Schedule
+SET Thumbnail = "No image available.png"
+WHERE Thumbnail IS NULL;
+
+/*Update customer names according to file system. If customer names don't match exactly then the Job_number Hyperlink won't work*/
+UPDATE Tooling_Schedule
+SET Customer = "Ada Metals"
+WHERE Customer = "ADA Metal Products Inc";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Aeroparts'
+WHERE Customer = 'AeroParts Manufacturing & Repair';
+
+UPDATE Tooling_Schedule
+SET Customer = "Alabama Laser"
+WHERE Customer = "Alabama Laser Systems";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Arvin Sango, Inc'
+WHERE Customer = 'ARVIN SANGO, INC';
+
+UPDATE Tooling_Schedule
+SET Customer = 'California Hydroforming'
+WHERE Customer = 'CALIFORNIA HYDROFORMING CO INC';
+
+UPDATE Tooling_Schedule
+SET Customer = "Cardington Yutaka"
+WHERE Customer = "Cardington Yutaka Technologies";
+
+UPDATE Tooling_Schedule
+SET Customer = "Carnegie Robotics"
+WHERE Customer = "Carnegie Robotics, LLC";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Case NH'
+WHERE Customer = 'CASE NEW HOLLAND';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Caterpillar'
+WHERE Customer = 'CATERPILLAR INC.';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Chip Ganassi Racing'
+WHERE Customer = 'CHIP GANASSI RACING';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Chrysler'
+WHERE Customer = 'CHRYSLER COMPANY LLC';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Cobra Patterns & Models'
+WHERE Customer = 'COBRA PATTERNS & MODELS INC';
+
+UPDATE Tooling_Schedule
+SET Customer = 'ContiTech'
+WHERE Customer = 'CONTITECH';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Cummins'
+WHERE Customer = 'CUMMINS EMISSION SOLUTIONS';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Depatie'
+WHERE Customer = 'DEPATIE FLUID POWER COMPANY';
+
+UPDATE Tooling_Schedule
+SET Customer = "Eaton"
+WHERE Customer = "Eaton Corporation";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Eberspaecher'
+WHERE Customer = 'EBERSPAECHER N.A.';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Electrolux'
+WHERE Customer = 'Electro Lux';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Exhaust Systems'
+WHERE Customer = 'EXHAUST SYSTEMS';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Faurecia'
+WHERE Customer = 'FAURECIA EMISSIONS CONTROL';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Ford'
+WHERE Customer = 'FORD MOTOR COMPANY';
+
+UPDATE Tooling_Schedule
+SET Customer = 'GMP Metal Products'
+WHERE Customer = 'GMP METAL PRODUCTS';
+
+UPDATE Tooling_Schedule 
+SET Customer = 'Hendrick Motorsports'
+WHERE Customer = "HENDRICK MOTORSPORTS";
+
+UPDATE Tooling_Schedule 
+SET Customer = "Hendrickson Int'l"
+WHERE Customer = "Hendrickson International";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Honda'
+WHERE Customer = 'HONDA R&D AMERICAS';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Humanetics'
+WHERE Customer = 'HUMANETICS INNOVATIVE SOLUTION';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Isuzu'
+WHERE Customer = 'ISUZU MANUFACTURING SERVICES';
+
+UPDATE Tooling_Schedule
+SET Customer = 'JAC Products'
+WHERE Customer = 'JAC PRODUCTS';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Jedco'
+WHERE Customer = 'JEDCO, INC';
+
+UPDATE Tooling_Schedule
+SET Customer = 'John Deere'
+WHERE Customer = 'JOHN DEERE HARVESTER WORKS';
+
+UPDATE Tooling_Schedule
+SET Customer = "Kellog"
+WHERE Customer = "Kellogg's";
+
+UPDATE Tooling_Schedule
+SET Customer = "Mack Trucks"
+WHERE Customer = "Mack Trucks, Inc";
+
+UPDATE Tooling_Schedule
+SET Customer = "Magneti"
+WHERE Customer = "Magneti Marelli";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Matcor'
+WHERE Customer = 'MATCOR MATSU';
+
+UPDATE Tooling_Schedule
+SET Customer = 'MSI Defense'
+WHERE Customer = 'MSI DEFENSE SOLUTIONS';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Newman Tech'
+WHERE Customer = 'NEWMAN TECHNOLOGY, INC';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Nikola'
+WHERE Customer = 'NIKOLA CORPORATION';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Penske'
+WHERE Customer = 'PENSKE RACING SOUTH INC';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Pit Products'
+WHERE Customer = 'PIT PRODUCTS LLC.';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Pridgeon & Clay'
+WHERE Customer = 'PRIDGEON & CLAY';
+
+UPDATE Tooling_Schedule
+SET Customer = "ProFab"
+WHERE Customer = "ProFab Production Fabricators, Inc.";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Richard Childress Racing'
+WHERE Customer = 'RCR RACE OPERATIONS, LLC';
+
+UPDATE Tooling_Schedule
+SET Customer = "Rivian"
+WHERE Customer = "RIVIAN";
+
+UPDATE Tooling_Schedule
+SET Customer = "Senior Flexonics"
+WHERE Customer = "Senior Flexonics Inc";
+
+UPDATE Tooling_Schedule
+SET Customer = "Senneker"
+WHERE Customer = "Senneker Performance";
+
+UPDATE Tooling_Schedule
+SET Customer = "Stryker"
+WHERE Customer = "Stryker Corporation";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Technique Chassis'
+WHERE Customer = 'TECHNIQUE CHASSIS';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Tenneco'
+WHERE Customer = 'TENNECO GRASS LAKE';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Tenneco Inc'
+WHERE Customer = 'TENNECO, INC';
+
+UPDATE Tooling_Schedule
+SET Customer = "Thor Sport Racing"
+WHERE Customer = "ThorSport Racing";
+
+UPDATE Tooling_Schedule
+SET Customer = 'Thyssenkrupp'
+WHERE Customer = 'THYSSENKRUPP NA';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Tramec Sloan'
+WHERE Customer = 'TRAMEC SLOAN(CAT)';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Volvo'
+WHERE Customer = 'VOLVO TRUCKS N. AMERICA';
+
+UPDATE Tooling_Schedule
+SET Customer = 'World Class Industries'
+WHERE Customer = 'WORLD CLASS INDUSTRIES';
+
+UPDATE Tooling_Schedule
+SET Customer = 'Yamaha'
+WHERE Customer = 'YAMAHA MOTOR MFG. CORP';
+
+UPDATE Tooling_Schedule
+SET Customer = 'ZF NA'
+WHERE Customer = 'ZF NORTH AMERICA';
