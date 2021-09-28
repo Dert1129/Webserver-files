63735064951bd1bc4ace4ab7b6524e9feb23536c
Dert1129 <nathancreger1111@gmail.com>
2021-09-28 17:29:54 -0400

    Implemented Tooling function

---
+++ routes/Tooling_Schedule.php
@@ -0,0 +1,42 @@
+<?php 
+function Tooling(){
+    require('../includes/dbh.inc.php');
+    $stmt = $conn->prepare("SELECT * FROM Tooling_Schedule;");
+    $stmt->execute();
+    $result = $stmt->get_result(); 
+    if ($result !==false){
+        while ($row = $result->fetch_assoc()){
+            $date = date_format(new DateTime($row["Due_Date"]), "Y-m-d");
+            $current_date = date("Y-m-d");
+            $year = date("Y");
+            $pastYear = $year - 1;
+            $path = '//tiws07/dwg/Customer/'.$year . '/' . $row['Customer'].'/Jobs/'.$row['Job_number'];
+            $altPath = '//tiws07/dwg/Customer/'.$pastYear . '/' . $row['Customer'].'/Jobs/'.$row['Job_number'];
+            if(is_dir($path)){
+                $directory = "file://///tiws07/dwg/Customer/".$year."/".$row['Customer']. "/Jobs/". $row['Job_number'];
+                $job = "<a href=\"$directory"."\"> " . $row['Job_number'] . " </a> </td>";
+            }elseif(is_dir($altPath)){
+                $directory = "file://///tiws07/dwg/Customer/".$pastYear."/".$row['Customer']. "/Jobs/". $row['Job_number'];
+                $job = "<a href=\"$directory"."\"> " . $row['Job_number'] . " </a> </td>";
+            }else{
+                $job = $row['Job_number'];
+            }
+            echo "<tr>";
+            if($date < $current_date){
+                $text = "text-danger";
+            }else{
+                $text = "";
+            }
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Customer']. "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$job."</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$date. "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Part_Number']. "</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Qty_To_Stock']."</td>";  
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Design_Complete']."</td>";
+            echo "<td class='col-1 $text' style='height:8rem'>".$row['Work_Center']."</td>";
+        }
+    }else{
+        die("Connection failed: ". mysqli_connect_error());
+    }
+}
+?>
