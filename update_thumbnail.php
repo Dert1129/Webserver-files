428ba6fe8d7a0a8e382dba86cc4c581798280134
Dert1129 <nathancreger1111@gmail.com>
2021-10-29 12:05:52 -0400

    Implemeted Thumbnail changing upon script execute

--- update_thumbnail.php
+++ update_thumbnail.php
@@ -1,34 +1,53 @@
 <?php
-    require("//tiws07/DWG/Mfg Mtg/Nathan/Webserver files/includes/dbh.inc.php");
-    $stmt = $conn->prepare("SELECT * FROM Job_Schedule;");
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if($result !== false){
-        while($row = $result->fetch_assoc()){
-            $date = date_format(new DateTime($row["Due_Date"]), "Y-m-d");
-            $current_date = date("Y-m-d");
-            if($date < $current_date){
-                $text = "text-danger";
-            }else{
-                $text = "";
-            }
-            if(strlen($row["Part_Number"])==2){
-                $stmt = $conn->prepare("UPDATE Job_Schedule SET Thumbnail = ? WHERE Part_Number = ?;");
-                $stmt->bind_param("ss",$Thumbnail, $row['Part_Number']);
-                $Thumbnail = $row['Part_Number'];
-                $stmt->execute();
-                $Thumbnail = "<td class='col-1 $text' style='height:8rem'>".$row['Thumbnail']."</td>";
-            }elseif ($row['Part_Number']=="PACKAGING") {
-                $stmt = $conn->prepare("UPDATE Job_Schedule SET Thumbnail = ? WHERE Part_Number = ?;");
-                $stmt->bind_param("ss",$Packaging,$row['Part_Number']);
-                $Packaging = "PACKAGING";
-                $stmt->execute();
-                $Thumbnail = "<td class='col-1 $text' style='height:8rem'>".$row['Thumbnail']."</td>";
-            }elseif($row['Thumbnail']=="No image available.png"){
-                $Thumbnail = "<td class='col-2'>". "<img class='lozad' id='Thumbnail' data-src='./Thumbnails/".$row['Thumbnail']."' width='170px' height='112px'>". "</td>";
-            }else{
-                $Thumbnail = "<td class='col-2'>". "<img class='lozad' data-src='./Thumbnails/".$row['Thumbnail']."' width='170px' height='112px'>". "</td>";
+    function Table ($tablename){
+        require("//tiws07/DWG/Mfg Mtg/Nathan/Webserver files/includes/dbh.inc.php");
+        $stmt = $conn->prepare("SELECT * FROM $tablename;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result !== false){
+            while($row = $result->fetch_assoc()){
+                $date = date_format(new DateTime($row["Due_Date"]), "Y-m-d");
+                $current_date = date("Y-m-d");
+                if($date < $current_date){
+                    $text = "text-danger";
+                }else{
+                    $text = "";
+                }
+                $files = scandir('//tiws07/DWG/Mfg Mtg/Nathan/Webserver files/Thumbnails/');
+                foreach ($files as $file) {
+                  if ($file !== "." && $file !== "..") {
+                    $name = strstr($file, '.png', TRUE);
+                    if($row['Part_Number']==$name){
+                        $stmt = $conn->prepare("UPDATE $tablename SET Thumbnail = ? WHERE Part_Number = ?;");
+                        $stmt->bind_param("ss", $file, $name);
+                        $files = scandir('//tiws07/DWG/Mfg Mtg/Nathan/Webserver files/Thumbnails/');
+                        $stmt->execute();
+                       }
+                     }
+                   }
+                if(strlen($row["Part_Number"])==2){
+                    $stmt = $conn->prepare("UPDATE $tablename SET Thumbnail = ? WHERE Part_Number = ?;");
+                    $stmt->bind_param("ss",$Thumbnail, $row['Part_Number']);
+                    $Thumbnail = $row['Part_Number'];
+                    $stmt->execute();
+                    $Thumbnail = "<td class='col-1 $text' style='height:8rem'>".$row['Thumbnail']."</td>";
+                }elseif ($row['Part_Number']=="PACKAGING") {
+                    $stmt = $conn->prepare("UPDATE $tablename SET Thumbnail = ? WHERE Part_Number = ?;");
+                    $stmt->bind_param("ss",$Packaging,$row['Part_Number']);
+                    $Packaging = "PACKAGING";
+                    $stmt->execute();
+                    $Thumbnail = "<td class='col-1 $text' style='height:8rem'>".$row['Thumbnail']."</td>";
+                }elseif($row['Thumbnail']=="No image available.png"){
+                    $Thumbnail = "<td class='col-2'>". "<img class='lozad' id='Thumbnail' data-src='./Thumbnails/".$row['Thumbnail']."' width='170px' height='112px'>". "</td>";
+                }else{
+                    $Thumbnail = "<td class='col-2'>". "<img class='lozad' data-src='./Thumbnails/".$row['Thumbnail']."' width='170px' height='112px'>". "</td>";
+                }
             }
         }
     }
+    $tablename = "Job_Schedule";
+    table($tablename);
+
+    $tablename = '2D_Laser_Schedule';
+    table($tablename);
 ?>
