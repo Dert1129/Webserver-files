84e22eb617877aaa9357ad881050003f52f6ab38
Dert1129 <nathancreger1111@gmail.com>
2021-11-01 14:29:35 -0400

    Added all schedules

--- execute_sql.php
+++ execute_sql.php
@@ -18,7 +18,16 @@ function Customer_Schedule($Filename){
         }
     }
     Customer_Schedule("Do everything Script.sql");
-    //Customer_Schedule("Brake & Baltec.sql");
+    /*Customer_Schedule("Brake & Baltec.sql");
+    Customer_Schedule("2D_Laser_Schedule.sql");
+    Customer_Schedule("Rolled_Sheet_Schedule.sql");
+    Customer_Schedule("Spacegear_schedule.sql");
+    Customer_Schedule("Stamping_Schedule.sql");
+    Customer_Schedule("Tooling_Parts_Schedule.sql");
+    Customer_Schedule("Tooling_Schedule.sql");
+    Customer_Schedule("Tube_Bender_Schedule.sql");
+    Customer_Schedule("Tube_Laser_Schedule.sql");
+    Customer_Schedule("WeldAssy_Schedule.sql");*/
     date_default_timezone_set("America/Detroit");
     file_put_contents ('//tiws07/dwg/Mfg Mtg/Nathan/Webserver Files/update.html', '<link href="./Style and cleanup/txtstyle.css" rel="stylesheet" type="text/css" />'."\n"."Last Updated: ".date("Y-m-d")." at ".date("g:iA"));
     echo "Updated Timestamp";
