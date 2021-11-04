31ae4b1b6617f72c99e2291ea0c5ea020141aa71
Dert1129 <nathancreger1111@gmail.com>
2021-11-04 17:26:40 -0400

    moved comments

--- ExecuteMe/MoveCsv.ps1
+++ ExecuteMe/MoveCsv.ps1
@@ -2,16 +2,16 @@ Function MoveCsv($FileName){
     Remove-Item -Path "C:\xampp\mysql\data\webserver\$FileName.csv" -Force
     Move-Item -Path "\\tiws07\dwg\Mfg Mtg\Customer Schedule\$FileName.csv" -Destination "C:\xampp\mysql\data\webserver\" -Force
 }
-#$FileName = "Job Schedule Details"
-#MoveCsv -File $FileName
+$FileName = "Job Schedule Details"
+MoveCsv -File $FileName
 #$FileName = "TubeLaser_Schedule"
 #MoveCsv -File $FileName
 #$FileName = "Tooling_TLs_Schedule"
 #MoveCsv -File $FileName
 #$FileName = "Tooling_Parts_Schedule"
 #MoveCsv -File $FileName
-$FileName = "Stamping_Schedule"
-MoveCsv -File $FileName
+#$FileName = "Stamping_Schedule"
+#MoveCsv -File $FileName
 #$FileName = "Rolled Shells_Schedule"
 #MoveCsv -File $FileName
 #$FileName = "Job Schedule Details"
